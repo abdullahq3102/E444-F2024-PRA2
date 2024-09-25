8c43c60d74a71ca59072f80ccfde5c7ac0166ad4
Abdullah Qureshi <abd.qureshi@mail.utoronto.ca>
2024-09-25 14:20:21 -0400

    finish 1.3

--- hello.py
+++ hello.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # current_time = datetime.utcnow()
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('user/<username>')
-def show_user_profile(username):
-    return 'Hello, %s' % username
+@app.route('/user/<name>')
+def user(name):
+    # current_time = datetime.utcnow()
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
