f08448faca88b754f7d55bfb2b184a2d6bd957f5
Abdullah Qureshi <abd.qureshi@mail.utoronto.ca>
2024-09-25 13:23:09 -0400

    example 2-2

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('user/<username>')
+def show_user_profile(username):
+    return 'Hello, %s' % username
